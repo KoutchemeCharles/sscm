2d16c676a8509e203da9dabe06c92dad9b453dd4
Charles Koutcheme <charles.koutcheme@aalto.fi>
2023-02-13 16:20:12 +0200

    updating function extraction procedure

--- extract.py
+++ extract.py
@@ -1,6 +1,9 @@
 from tokenize_rt import tokens_to_src, src_to_tokens 
 
 def is_parsable(upload):
+    """ Checks whether or not a string
+    is parsable into python tokens. """
+    
     try:
         src_to_tokens(upload)
         return True
@@ -8,6 +11,8 @@ def is_parsable(upload):
         return False
 
 def find_next_indent(tokens):
+    """ Finds the first INDENT token. """
+    
     for i, token_info in enumerate(tokens):
         if token_info.name == "INDENT":
             return i
@@ -15,6 +20,9 @@ def find_next_indent(tokens):
     return len(tokens) - 1 if len(tokens) > 0 else 0 
 
 def find_last_dedent(tokens):
+    """ Finds the position of the last dedent token scoping
+    the function within the tokens. """
+    
     n_indent = 1
     for i, token_info in enumerate(tokens):
         
@@ -29,6 +37,10 @@ def find_last_dedent(tokens):
     return None
 
 def scope_function(start_index, tokens):
+    """ Given a starting index and a list of tokens of a source code,
+    find the index at which there is the end of the first function
+    encountered. """
+    
     start_search = find_next_indent(tokens[start_index:]) + start_index + 1
     end_search = find_last_dedent(tokens[start_search:]) 
     # if there are no dedent, I should just finnish at the last one 
@@ -41,6 +53,10 @@ def scope_function(start_index, tokens):
 
 
 def find_functions(tokens):
+    """ Analyze the sequences of tokens of a source code to
+    and yields the list of tokens of the functions defined
+    inside that source code. """
+    
     for i, token_info in enumerate(tokens):
         if token_info.src == "def":
             start_index = i
@@ -50,12 +66,17 @@ def find_functions(tokens):
             yield func_tokens
             
 def get_function_name(tokens):
+    """ Given a list of tokenize_rt.tokens of a function, 
+    return the name of the function. """
     for t in tokens:
         if t.name == 'NAME' and t.src != "def":
             return t.src 
     return ""
 
 def get_predicted_function(completion, f_name):
+    """ Obtain the first defined function in the source code which
+    has that function name. """
+    
     information = parse_upload(completion)
     for info in information:
         if info["name"] == f_name:
@@ -63,6 +84,8 @@ def get_predicted_function(completion, f_name):
     return ""
 
 def parse_upload(upload):
+    """ Parse the submitted student code into a list of 
+    functions with their tokens. """
     
     try:
         token_infos = src_to_tokens(upload) # The tokens infos
@@ -115,5 +138,5 @@ def extract_relevant_part(solution, function_name):
         if not is_parsable(new_string):
             raise ValueError("Routine extraction failed for solution", solution)
         
-        return main_routine 
+        return new_string 
         
